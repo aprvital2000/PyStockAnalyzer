42c2ee9df718e58b1bd09927e146ccea48c0bbc3
aprvital2000 <107157324+aprvital2000@users.noreply.github.com>
2023-09-12 08:35:59 -0700

    Added code to read file if exists for a given day

--- analyse.py
+++ analyse.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os.path
+from pathlib import Path
+from datetime import date
+
 plt.style.use('default')
 pd.options.display.max_rows = 100
 
+# Vital
 api_key = 'RUUKVAG9C6D2ECAL'
+
+# Vishnu
+# api_key = 'EE45PHWQN0W27PS1'
+
 plot_chart = False
-print_result = False
+print_result = True
 
 # MANAMANA
 symbols = ['MSFT', 'AAPL', 'NFLX', 'AMZN', 'META', 'ADBE', 'NVDA', 'GOOGL', 'TLSA']
@@ -16,17 +25,26 @@ output_size = 'full' #default - compact 100 days data only
 data_type = 'csv' #default - json
 
 def analyzeSymbol(symbol):
-    print("analyzeSymbol == " + symbol)
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey={}&symbol={}&outputsize={}&datatype={}'
-    url = url.format(api_key, symbol, output_size, data_type)
-    print("URL --> %s" % url)
+    print("analyzeSymbol --> %s" %symbol)
+    file_path = getFilePath(symbol)
+    file_exists = os.path.isfile(file_path)
+
+    url = file_path
+    if not file_exists:
+        url = getApiUrl(symbol)
+    print("Get data from URL --> %s" %url)
 
     df = pd.read_csv(url)
-    print("Got nRecords ==  %d" % df.size)
+    print("Got nRecords ==  %d" %df.size)
+    if df.size < 5:
+        print("Error getting data from URL --> %s" %url)
+
     print(df.head(5))
     df = df.truncate(after=data_truncate_days)
     df['timestamp'] = pd.to_datetime(df['timestamp'])
-    print(df.head(10))
+
+    if not file_exists:
+        df.to_csv(file_path)
 
     close = df['close']
     ### Calculate Exponential Moving Averages (EMA)
@@ -54,5 +72,13 @@ def analyzeSymbol(symbol):
 def analyzeSymbols():
     for symbol in symbols:
         analyzeSymbol(symbol)
+
+def getFilePath(symbol):
+    file_path = '/Users/aprvital/Documents/Code/PyStockAnalysis/{}-{}.csv'
+    return file_path.format(symbol, date.today().strftime('%d%m%y'))
+
+def getApiUrl(symbol):
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey={}&symbol={}&outputsize={}&datatype={}'
+    return url.format(api_key, symbol, output_size, data_type)
 
 analyzeSymbols()
