5edf2d1676532f56ac7bf231ddc645d8ebc031e4
aprvital2000 <107157324+aprvital2000@users.noreply.github.com>
2023-09-13 16:41:13 -0700

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os.path
-from pathlib import Path
+import numpy as np
 from datetime import date
 
 plt.style.use('default')
-pd.options.display.max_rows = 100
 
 # Vital
 api_key = 'RUUKVAG9C6D2ECAL'
@@ -13,8 +12,9 @@ api_key = 'RUUKVAG9C6D2ECAL'
 # Vishnu
 # api_key = 'EE45PHWQN0W27PS1'
 
-plot_chart = True
+plot_chart = True 
 print_result = False
+write_to_file = True
 
 # MANAMANA
 symbols = ['MSFT', 'AAPL', 'NFLX', 'AMZN', 'META', 'ADBE', 'NVDA', 'GOOGL', 'TLSA']
@@ -23,44 +23,54 @@ rsi_rp = 14
 bbands_rp = 20
 stoch_rp_1 = 14
 stoch_rp_2 = 3
+adx_rp = 14
 
-output_size = 'full'  # default - compact 100 days data only
-data_type = 'csv'  # default - json
+output_size = 'full' # default - compact 100 days data only
+data_type = 'csv' # default - json
 
-csv_file_url = 'C:/Users/panumula/Downloads/data-analysis/{}-{}.csv'
+file_dir = 'C:/Users/panumula/Downloads/data-analysis'
+dest_csv_file_url = '{}/{}-enriched-{}.csv'
+src_csv_file_url = '{}/{}-{}.csv'
+
 api_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey={}&symbol={}&outputsize={}&datatype={}'
 
-
 def analyzeSymbol(symbol):
-    print("analyzeSymbol --> %s" % symbol)
-    file_path = csv_file_url.format(symbol, date.today().strftime('%d%m%y'))
-    file_exists = os.path.isfile(file_path)
+    print('Analyzing Symbol: %s' % symbol)
+    src_file_path = src_csv_file_url.format(file_dir, symbol, date.today().strftime('%d%m%y'))
+    file_exists = os.path.isfile(src_file_path)
 
-    url = file_path
+    url = src_file_path
     if not file_exists:
         url = api_url.format(api_key, symbol, output_size, data_type)
-    print("Get data from URL --> %s" % url)
+    print('Get data from URL: %s' % url)
 
     df = pd.read_csv(url)
-    print("Got nRecords ==  %d" % df.size)
+    print('Got nRecords: %d' % df.size)
 
     if df.size < 5:
-        print("Error getting data from URL --> %s" % url)
+        print('Error getting data from URL: %s' % url)
+        return
 
     print(df.head(5))
     df = df.truncate(after=data_truncate_days)
     df['timestamp'] = pd.to_datetime(df['timestamp'])
 
     if not file_exists:
-        df.to_csv(file_path)
+        df.to_csv(src_file_path, index=False)
 
     typicalPrice(df)
     macd(df)
-    doubleEMA(df)
-    relativeStrengthIndicator(df)
+    dema(df)
+    rsi(df)
     bollingerBand(df)
-    stochasticOscillator(df)
-    volumeWeightedAvgPrice(df)
+    stoch(df)
+    vwap(df)
+    adx(df)
+    obv(df)
+
+    if(write_to_file):
+        dest_file_path = dest_csv_file_url.format(file_dir, symbol, date.today().strftime('%d%m%y'))
+        df.to_csv(dest_file_path, index=False)
 
     if print_result:
         print(df.to_string())
@@ -68,12 +78,11 @@ def analyzeSymbol(symbol):
     if plot_chart:
         plotCharts(df, symbol)
 
-
 def analyzeSymbols():
     for symbol in symbols:
         analyzeSymbol(symbol)
 
-# Implement ADX, OBV, CCI, AROON
+# TODO - Implement CCI, AROON
 
 # TP
 def typicalPrice(df):
@@ -81,7 +90,7 @@ def typicalPrice(df):
     return df
 
 # STOCH
-def stochasticOscillator(df):
+def stoch(df):
     low_min = df['low'].rolling(window=stoch_rp_1).min().shift(-stoch_rp_1+1)
     high_max = df['high'].rolling(window=stoch_rp_1).max().shift(-stoch_rp_1+1)
     k_fast = 100 * (df['close'] - low_min)/(high_max - low_min)
@@ -94,12 +103,12 @@ def stochasticOscillator(df):
     return df
 
 # VWAP
-def volumeWeightedAvgPrice(df):
+def vwap(df):
     df['vwap'] = (df['tp'] * df['volume']).cumsum() / df['volume'].cumsum()
     return df
 
 # RSI
-def relativeStrengthIndicator(df):
+def rsi(df):
     delta = df['close'].diff()
     gain = delta.where(delta > 0, 0)
     loss = -delta.where(delta < 0, 0)
@@ -131,7 +140,7 @@ def macd(df):
     return df
 
 # DEMA
-def doubleEMA(df):
+def dema(df):
     ema20 = df['close'].ewm(span=20).mean()
     eema20 = ema20.ewm(span=20).mean()
     dema20 = 2 * ema20 - eema20
@@ -163,48 +172,99 @@ def bollingerBand(df):
     df['bl'] = bl
     return df
 
+# ADX
+def adx(df):
+    high = df['high']
+    low = df['low']
+    close = df['close']
+    plus_dm = high.diff()
+    minus_dm = low.diff()
+    plus_dm[plus_dm < 0] = 0
+    minus_dm[minus_dm > 0] = 0
+    
+    tr1 = pd.DataFrame(high - low)
+    tr2 = pd.DataFrame(abs(high - close.shift(1)))
+    tr3 = pd.DataFrame(abs(low - close.shift(1)))
+    frames = [tr1, tr2, tr3]
+    tr = pd.concat(frames, axis = 1, join = 'inner').max(axis = 1)
+    atr = tr.rolling(adx_rp).mean()
+    
+    plus_di = 100 * (plus_dm.ewm(alpha = 1/adx_rp).mean() / atr)
+    minus_di = abs(100 * (minus_dm.ewm(alpha = 1/adx_rp).mean() / atr))
+    dx = (abs(plus_di - minus_di) / abs(plus_di + minus_di)) * 100
+    adx = ((dx.shift(1) * (adx_rp - 1)) + dx) / adx_rp
+    adx_smooth = adx.ewm(alpha = 1/adx_rp).mean()
 
+    df['plus_di'] = plus_di
+    df['minus_di'] = minus_di
+    df['adx_smooth'] = adx_smooth
+    df['adx_reco'] = 'None'
+    df.loc[(adx_smooth > 25) & (plus_di < minus_di), 'adx_reco'] = 'Sell'
+    df.loc[(adx_smooth > 25) & (plus_di > minus_di), 'adx_reco'] = 'Buy'
+    
+    return df
+
+# OBV
+def obv(df):
+    df['obv'] = np.where(df['close'] > df['close'].shift(1), df['volume'], np.where(df['close'] < df['close'].shift(1), -df['volume'], 0)).cumsum()
+    return df
+
+# Plot Charts
 def plotCharts(df, symbol):
-    fig, ax = plt.subplots(nrows=6, ncols=1, sharex=True, figsize=(18, 8))
+    fig, ax = plt.subplots(nrows=4, ncols=2, sharex=True, figsize=(18, 8))
 
-    ax[0].plot(df['timestamp'], df['macd'], label='macd', color='#ff000080', linestyle='solid')
-    ax[0].plot(df['timestamp'], df['macd_signal'], label='macd_signal', color='#0000ff80', linestyle='-.')
-    ax[0].set_ylabel('MACD')
-    ax[0].grid(True, linestyle='-.')
-    ax[0].legend()
+    ax[0, 0].plot(df['timestamp'], df['macd'], label='macd', color='#ff000080', linestyle='solid')
+    ax[0, 0].plot(df['timestamp'], df['macd_signal'], label='macd_signal', color='#0000ff80', linestyle='-.')
+    ax[0, 0].set_ylabel('MACD')
+    ax[0, 0].grid(True, linestyle='-.')
+    ax[0, 0].legend()
 
-    ax[1].plot(df['timestamp'], df['dema20'], label='dema20', color='#ff000080', linestyle='solid')
-    ax[1].plot(df['timestamp'], df['dema50'], label='dema50', color='#0000ff80', linestyle='-.')
-    ax[1].set_ylabel('DEMA')
-    ax[1].grid(True, linestyle='-.')
-    ax[1].legend()
+    ax[1, 0].fill_between(df['timestamp'], df['rsi'], label='rsi', color='lightsteelblue', linestyle='solid')
+    ax[1, 0].axhline(y=30, color='#00ff0080', label='30%', linestyle='dotted', lw=1)
+    ax[1, 0].axhline(y=50, color='#0000ff80', label='50%', linestyle='dotted', lw=1)
+    ax[1, 0].axhline(y=70, color='#ff000080', label='70%', linestyle='dotted', lw=1)
+    ax[1, 0].set_ylabel('RSI')
+    ax[1, 0].grid(True, linestyle='-.')
+    ax[1, 0].legend()
 
-    ax[2].fill_between(df['timestamp'], df['rsi'], label='rsi', color='lightsteelblue', linestyle='solid')
-    ax[2].axhline(y=30, color='#00ff0080', label='30%', linestyle='dotted', lw=1)
-    ax[2].axhline(y=50, color='#0000ff80', label='50%', linestyle='dotted', lw=1)
-    ax[2].axhline(y=70, color='#ff000080', label='70%', linestyle='dotted', lw=1)
-    ax[2].set_ylabel('RSI')
-    ax[2].grid(True, linestyle='-.')
-    ax[2].legend()
+    ax[2, 0].plot(df['timestamp'], df['k_fast'], label='k_fast', color='#ff000080', linestyle='solid')
+    ax[2, 0].plot(df['timestamp'], df['k_slow'], label='k_slow', color='#0000ff80', linestyle='-.')
+    ax[2, 0].set_ylabel('STOCH')
+    ax[2, 0].grid(True, linestyle='-.')
+    ax[2, 0].legend()
 
-    ax[3].plot(df['timestamp'], df['close'], label='closing price', color='#0000ff80', linestyle='solid')
-    ax[3].plot(df['timestamp'], df['bu'], label='bollinger upper', color='#ff000080', linestyle='dotted')
-    ax[3].plot(df['timestamp'], df['bl'], label='bollinger lower', color='#00ff0080', linestyle='dotted')
-    ax[3].set_ylabel('BOLLINGER')
-    ax[3].grid(True, linestyle='-.')
-    ax[3].legend()
+    ax[3, 0].plot(df['timestamp'], df['dema20'], label='dema20', color='#ff000080', linestyle='solid')
+    ax[3, 0].plot(df['timestamp'], df['dema50'], label='dema50', color='#0000ff80', linestyle='-.')
+    ax[3, 0].set_ylabel('DEMA')
+    ax[3, 0].grid(True, linestyle='-.')
+    ax[3, 0].legend()
 
-    ax[4].plot(df['timestamp'], df['k_fast'], label='k_fast', color='#ff000080', linestyle='solid')
-    ax[4].plot(df['timestamp'], df['k_slow'], label='k_slow', color='#0000ff80', linestyle='-.')
-    ax[4].set_ylabel('STOCH')
-    ax[4].grid(True, linestyle='-.')
-    ax[4].legend()
+    ax[0, 1].plot(df['timestamp'], df['close'], label='closing price', color='#0000ff80', linestyle='solid')
+    ax[0, 1].plot(df['timestamp'], df['bu'], label='bollinger upper', color='#ff000080', linestyle='dotted')
+    ax[0, 1].plot(df['timestamp'], df['bl'], label='bollinger lower', color='#00ff0080', linestyle='dotted')
+    ax[0, 1].set_ylabel('BOLLINGER')
+    ax[0, 1].grid(True, linestyle='-.')
+    ax[0, 1].legend()
 
-    ax[5].plot(df['timestamp'], df['close'], label='closing price', color='#0000ff80', linestyle='-.')
-    ax[5].plot(df['timestamp'], df['vwap'], label='vwap', color='#ff000080', linestyle='solid')
-    ax[5].set_ylabel('VWAP')
-    ax[5].grid(True, linestyle='-.')
-    ax[5].legend()
+    ax[1, 1].plot(df['timestamp'], df['close'], label='closing price', color='#0000ff80', linestyle='-.')
+    ax[1, 1].plot(df['timestamp'], df['vwap'], label='vwap', color='#ff000080', linestyle='solid')
+    ax[1, 1].set_ylabel('VWAP')
+    ax[1, 1].grid(True, linestyle='-.')
+    ax[1, 1].legend()
+
+    ax[2, 1].plot(df['timestamp'], df['obv'], label='obv', color='#ff000080', linestyle='solid')
+    ax[2, 1].plot(df['timestamp'], df['volume'], label='volume', color='#00ff0080', linestyle='solid')
+    ax[2, 1].set_ylabel('OBV')
+    ax[2, 1].grid(True, linestyle='-.')
+    ax[2, 1].legend()
+
+    ax[3, 1].plot(df['timestamp'], df['adx_smooth'], label='adx_smooth', color='#0000ff80', linestyle='solid')
+    ax[3, 1].plot(df['timestamp'], df['plus_di'], label='+di', color='#ff000080', linestyle='dotted')
+    ax[3, 1].plot(df['timestamp'], df['minus_di'], label='-di', color='#00ff0080', linestyle='dotted')
+    ax[3, 1].axhline(y=25, color='#00000080', label='25', linestyle='dotted', lw=1)
+    ax[3, 1].set_ylabel('ADX')
+    ax[3, 1].grid(True, linestyle='-.')
+    ax[3, 1].legend()
 
     fig.subplots_adjust(hspace=0)
     fig.suptitle(symbol)
@@ -213,6 +273,5 @@ def plotCharts(df, symbol):
 
     # plt.xticks(rotation=45)
     plt.show()
-
 
 analyzeSymbol(symbols[0])
