72b265b001cf8e097307a44888823249007594ce
aprvital2000 <107157324+aprvital2000@users.noreply.github.com>
2023-09-25 15:36:09 -0700

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -15,19 +15,18 @@ api_key = 'RUUKVAG9C6D2ECAL'
 # Vishnu
 # api_key = 'EE45PHWQN0W27PS1'
 
-plot_chart = False
-print_result = False
+print_result = True
 write_to_file = True
 purge_files_after_days = 1
 
-data_truncate_days = 300
+data_truncate_days = 250
 
 output_size = 'full'  # default - compact 100 days data only
 data_type = 'csv'  # default - json
 
 file_dir = os.getcwd()
-dest_csv_file_url = '{}/{}-enriched-{}.csv'
-src_csv_file_url = '{}/{}-{}.csv'
+dest_csv_file_url = '{}/data/{}-enriched-{}.csv'
+src_csv_file_url = '{}/data/{}-{}.csv'
 
 api_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey={}&symbol={}&outputsize={}&datatype={}'
 
@@ -69,27 +68,46 @@ def analyze_symbol(symbol):
     df.sort_index(ascending=True, inplace=True)
 
     df.ta.bbands(close='close', length=20, std=2, signal_indicators=True, append=True)
+    df['bb_reco'] = df.apply(lambda row: bb_reco(row), axis=1)
+
     df.ta.macd(close='close', signal_indicators=True, append=True)
+    macd_reco(df)
+    df['macd_reco'] = df.apply(lambda row: macd_reco2(row), axis=1)
+
     df.ta.roc(close='close', append=True)
+    upward_signal = ta.cross_value(df['ROC_10'], 0, above=True)
+    df['ROC_10_XU0'] = upward_signal
+    downward_signal = ta.cross_value(df['ROC_10'], 0, above=False)
+    df['ROC_10_XD0'] = downward_signal
+    df['roc_reco'] = df.apply(lambda row: roc_reco(row), axis=1)
+
     df.ta.rsi(close='close', signal_indicators=True, append=True)
-    df.ta.stc(close='close', append=True)
+    df['rsi_reco'] = df.apply(lambda row: rsi_reco(row), axis=1)
 
     df.ta.stoch(close='close', signal_indicators=True, append=True)
     xsignal = ta.cross(df['STOCHk_14_3_3'], df['STOCHd_14_3_3'])
     df['STOCHk_14_3_3_XA'] = xsignal
     xsignal = ta.cross(df['STOCHd_14_3_3'], df['STOCHk_14_3_3'])
     df['STOCHk_14_3_3_XB'] = xsignal
+    df['stoch_reco'] = df.apply(lambda row: stoch_reco(row), axis=1)
+
+    df.ta.aroon(high='high', low='low', append=True)
+    df['aroon_reco'] = df.apply(lambda row: aroon_reco(row), axis=1)
+
+    df.ta.adx(high='high', low='low', close='close', signal_indicators=True, append=True)
+    df['adx_reco'] = df.apply(lambda row: adx_reco(row), axis=1)
 
     df.ta.vwap(high='high', low='low', close='close', volume='volume', append=True)
-    df.ta.adx(high='high', low='low', close='close', signal_indicators=True, append=True)
-    df.ta.obv(close='close', volume='volume', append=True)
-    df.ta.cci(high='high', low='low', close='close', append=True)
-    df.ta.willr(high='high', low='low', close='close', append=True)
-    df.ta.aroon(high='high', low='low', append=True)
+    df['vwap_reco'] = df.apply(lambda row: vwap_reco(row), axis=1)
 
-    macd_reco(df)
-    adx_reco(df)
-    stoch_reco(df)
+    df.ta.obv(close='close', volume='volume', append=True)
+    df['obv_reco'] = df.apply(lambda row: obv_reco(row), axis=1)
+
+    df.ta.cci(high='high', low='low', close='close', append=True)
+    df['cci_reco'] = df.apply(lambda row: cci_reco(row), axis=1)
+
+    df.ta.willr(high='high', low='low', close='close', append=True)
+    df['willr_reco'] = df.apply(lambda row: willr_reco(row), axis=1)
 
     df.sort_index(ascending=False, inplace=True)
     df.dropna(subset=['MACDs_12_26_9'], inplace=True)
@@ -100,12 +118,54 @@ def analyze_symbol(symbol):
 
     if print_result:
         print(df.keys())
-        # print(df.to_string())
-
-    if plot_chart:
-        plot_charts(df, symbol)
+        print(df.head()[
+                  ['macd_reco', 'roc_reco', 'aroon_reco', 'rsi_reco', 'adx_reco', 'stoch_reco', 'bb_reco', 'cci_reco',
+                   'willr_reco', 'vwap_reco', 'obv_reco']])
 
     return df
+
+
+def macd_reco2(row):
+    # Logic To Be Verified
+    # When the MACD crosses down MACDs above 0, indicates downtrend
+    # When the MACD crosses up MACDs below 0, indicates uptrend
+    if (row['MACDh_12_26_9_XA_0'] == 1) & (row['MACD_12_26_9'] < 0):
+        return 'Buy'
+    elif (row['MACDh_12_26_9_XB_0'] == 1) & (row['MACD_12_26_9'] > 0):
+        return 'Sell'
+    return None
+
+
+def aroon_reco(row):
+    return 'Buy' if row['AROONOSC_14'] >= 0 else 'Sell'
+
+
+def roc_reco(row):
+    if row['ROC_10_XU0'] == 1:
+        return 'Buy'
+    elif row['ROC_10_XD0'] == 1:
+        return 'Sell'
+    return None
+
+
+def rsi_reco(row):
+    if row['RSI_14'] >= 70:
+        return 'Buy'
+    elif row['RSI_14'] <= 30:
+        return 'Sell'
+    return None
+
+
+def adx_reco(row):
+    # Logic To Be Verified
+    # ADX > 25 - Strong trend (But, No Direction)
+    # When the +DMI is above the -DMI, prices are moving up
+    # When the -DMI is above the +DMI, prices are moving down
+    if (row['ADX_14'] > 25) & (row['DMP_14'] > row['DMN_14']):
+        return 'Buy'
+    elif (row['ADX_14'] > 25) & (row['DMP_14'] < row['DMN_14']):
+        return 'Sell'
+    return None
 
 
 def macd_reco(df):
@@ -119,62 +179,53 @@ def macd_reco(df):
     return df
 
 
-def adx_reco(df):
-    # Logic To Be Verified
-    # ADX > 25 - Strong trend (But, No Direction)
-    # When the +DMI is above the -DMI, prices are moving up
-    # When the -DMI is above the +DMI, prices are moving down
-    df['adx_reco'] = 'No Action'
-    df.loc[(df['ADX_14'] > 25) & (df['DMP_14'] > df['DMN_14']), 'adx_reco'] = 'Buy'
-    df.loc[(df['ADX_14'] > 25) & (df['DMN_14'] > df['DMP_14']), 'adx_reco'] = 'Sell'
-
-
-def stoch_reco(df):
+def stoch_reco(row):
     # Logic To Be Verified
     # %K <= 20, Indicates Over sold, potential reversal / bounce - Buy
     # %K >= 80, Indicates Over bought, potential reversal / pull back - Sell
     # When %K crosses above %D from below, indicates uptrend
     # When %K crosses below %D from above, indicates downtrend
-    df['stoch_reco'] = 'No Action'
-    df.loc[(df['STOCHk_14_3_3'] <= 20) | (df['STOCHk_14_3_3_XA'] == 1), 'stoch_reco'] = 'Buy'
-    df.loc[(df['STOCHk_14_3_3'] >= 80) | (df['STOCHk_14_3_3_XB'] == 1), 'stoch_reco'] = 'Sell'
+    if (row['STOCHk_14_3_3'] <= 20) & (row['STOCHk_14_3_3_XA'] == 1):
+        return 'Buy'
+    elif (row['STOCHk_14_3_3'] >= 80) & (row['STOCHk_14_3_3_XB'] == 1):
+        return 'Sell'
+    return None
 
 
-# Plot Charts
-def plot_charts(df, symbol):
-    fig, ax = plt.subplots(nrows=4, ncols=1, sharex=True, figsize=(18, 8))
+def bb_reco(row):
+    if row['close'] <= row['BBL_20_2.0']:
+        return 'Buy'
+    elif row['close'] >= row['BBU_20_2.0']:
+        return 'Sell'
+    return None
 
-    # ax[3].plot(df['timestamp'], df['STC_10_12_26_0.5'], label='STC_10_12_26_0.5', color='#ff000080', linestyle='solid')
-    # ax[3].plot(df['timestamp'], df['STCmacd_10_12_26_0.5'], label='STCmacd_10_12_26_0.5', color='#0000ff80', linestyle='solid')
-    # ax[3].plot(df['timestamp'], df['STCstoch_10_12_26_0.5'], label='STCstoch_10_12_26_0.5', color='#00ff0080', linestyle='solid')
-    # ax[3].axhline(y=0, color='#00000080', label='0', linestyle='solid', lw=1)
-    # ax[3].axhline(y=25, color='#00000080', label='<25 - oversold', linestyle='solid', lw=1)
-    # ax[3].axhline(y=75, color='#00000080', label='>75 - overbought', linestyle='solid', lw=1)
-    # ax[3].set_ylabel('STC')
-    # ax[3].grid(True, linestyle='-.')
-    # ax[3].legend()
 
-    # ax[4].plot(df['timestamp'], df['DMP_14'], label='+di', color='#ff000080', linestyle='solid')
-    # ax[4].plot(df['timestamp'], df['ADX_14'], label='ADX', color='#00ff0080', linestyle='solid')
-    # ax[4].plot(df['timestamp'], df['DMN_14'], label='-di', color='#0000ff80', linestyle='solid')
-    # ax[4].axhline(y=25, color='#00000080', label='25', linestyle='dotted', lw=1)
-    # ax[4].set_ylabel('ADX')
-    # ax[4].grid(True, linestyle='-.')
-    # ax[4].legend()
+def cci_reco(row):
+    if row['CCI_14_0.015'] <= -100:
+        return 'Buy'
+    elif row['CCI_14_0.015'] >= 100:
+        return 'Sell'
+    return None
 
-    # ax[5].plot(df['timestamp'], df['STOCHk_14_3_3'], label='k', color='#ff000080', linestyle='solid')
-    # ax[5].plot(df['timestamp'], df['STOCHd_14_3_3'], label='d', color='#0000ff80', linestyle='solid')
-    # ax[5].set_ylabel('STOCH')
-    # ax[5].grid(True, linestyle='-.')
-    # ax[5].legend()
 
-    fig.subplots_adjust(hspace=0)
-    fig.suptitle(symbol)
-    # fig.supxlabel('Date')
-    fig.tight_layout()
+def willr_reco(row):
+    if row['WILLR_14'] <= -80:
+        return 'Buy'
+    elif row['WILLR_14'] >= -20:
+        return 'Sell'
+    return None
 
-    # plt.xticks(rotation=45)
-    plt.show()
+
+def vwap_reco(row):
+    if row['close'] >= row['VWAP_D']:
+        return 'Buy'
+    elif row['close'] < row['VWAP_D']:
+        return 'Sell'
+    return None
+
+
+def obv_reco(row):
+    return None
 
 
 def cleanup_files():
@@ -191,3 +242,6 @@ def cleanup_files():
         if (i.endswith('.csv')) & (file_time < time.time() - 86400 * purge_files_after_days):
             print(f" Delete : {i}")
             os.remove(file_location)
+
+# analyze_symbols()
+# analyze_symbol('AAPL')
